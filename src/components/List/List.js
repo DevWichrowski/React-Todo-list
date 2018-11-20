d71f16bbe2ee6fbe6b9e7ad9bdb77aef95215fe2
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-11-20 20:46:52 +0100

    Added button with modals where you can add new task with title and desc

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,16 +7,7 @@ export default class List extends Component {
 		super(props);
 
 		this.state = {
-			Items: [
-				{
-					title: 'test',
-					description: 'description'
-				},
-				{
-					title: 'tes2',
-					description: 'descriptio2'
-				}
-			],
+			Items: [],
 			tempTitle: '',
 			tempDesc: ''
 		};
@@ -61,24 +52,64 @@ export default class List extends Component {
 						<div className="item-list">{list}</div>
 					</div>
 				</div>
-				<div className="add-item-button">
-					<input
-						type="text"
-						className="form-control"
-						placeholder="Tytul"
-						aria-label="Tytul"
-						aria-describedby="basic-addon1"
-						onChange={this.saveTitle}
-					/>
-					<input
-						type="text"
-						className="form-control"
-						placeholder="Opis"
-						aria-label="Opis"
-						aria-describedby="basic-addon1"
-						onChange={this.saveDescription}
-					/>
-					<button onClick={this.addItemToList}>Dodaj</button>
+
+				<button
+					type="button"
+					class="btn btn-primary"
+					data-toggle="modal"
+					data-target="#exampleModal"
+					data-whatever="@mdo"
+				>
+					+
+				</button>
+				<div
+					class="modal fade"
+					id="exampleModal"
+					tabindex="-1"
+					role="dialog"
+					aria-labelledby="exampleModalLabel"
+					aria-hidden="true"
+				>
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">
+									Dodaj swój nowy task
+								</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+								<form>
+									<div class="form-group">
+										<label for="tytul" class="col-form-label">
+											Tytul:
+										</label>
+										<input type="text" class="form-control" id="tytul" onChange={this.saveTitle} />
+									</div>
+									<div class="form-group">
+										<label for="description" class="col-form-label">
+											Opis:
+										</label>
+										<textarea
+											class="form-control"
+											id="description"
+											onChange={this.saveDescription}
+										/>
+									</div>
+								</form>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">
+									Zamknij
+								</button>
+								<button type="button" class="btn btn-primary" onClick={this.addItemToList}>
+									Dodaj task
+								</button>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
