aa2d12687076791898a8e57cce64337892b50588
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-11-20 20:17:43 +0100

    Added React bootstrap to the project

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,10 +1,6 @@
-import React from 'react'
+import React from 'react';
 import './Item.css';
 
 export default function Item(props) {
-  return (
-    <div className="item">
-      {props.title}
-    </div>
-  )
+	return <div className="item">{props.title}</div>;
 }
