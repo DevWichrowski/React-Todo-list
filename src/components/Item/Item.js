0d222cd5c3c43a64b4a535c86ba1dcaeaa227a31
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2018-11-26 23:14:50 +0100

    Little of code sugar?

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -19,26 +19,24 @@ class Item extends Component {
 
 	handleEditItem = () => {
 		this.setState({ editItem: !this.state.editItem });
-		{this.props.clearTempState()}
+		this.props.clearTempState();
 	};
 
 	render() {
 		return (
 			<div>
 				<div className="item">
-				<div className="item-title">
-					{this.props.title}
-					</div>
+					<div className="item-title">{this.props.title}</div>
 					<div className="item-buttons">
-					<button className="btn btn-success" data-toggle="modal" onClick={this.handleModal}>
-						Look
-					</button>
-					<button className="btn btn-primary" onClick={this.handleEditItem}>
-						Edit
-					</button>
-					<button className="btn btn-danger" onClick={this.props.delete}>
-						Delete
-					</button>
+						<button className="btn btn-success" data-toggle="modal" onClick={this.handleModal}>
+							Look
+						</button>
+						<button className="btn btn-primary" onClick={this.handleEditItem}>
+							Edit
+						</button>
+						<button className="btn btn-danger" onClick={this.props.delete}>
+							Delete
+						</button>
 					</div>
 				</div>
 				<LookAtItem
@@ -47,15 +45,15 @@ class Item extends Component {
 					title={this.props.title}
 					description={this.props.description}
 				/>
-				<EditItem 
-				edit={this.state.editItem}
-				title={this.props.title}
-				description={this.props.description}
-				saveTitle={this.props.saveTitle}
-				saveDescription={this.props.saveDescription}
-				editItem={this.props.editItem}
-				handleEditItem={this.handleEditItem}
-				tempTitle={this.props.tempTitle}
+				<EditItem
+					edit={this.state.editItem}
+					title={this.props.title}
+					description={this.props.description}
+					saveTitle={this.props.saveTitle}
+					saveDescription={this.props.saveDescription}
+					editItem={this.props.editItem}
+					handleEditItem={this.handleEditItem}
+					tempTitle={this.props.tempTitle}
 				/>
 			</div>
 		);
